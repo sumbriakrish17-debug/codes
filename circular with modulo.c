dfb9f7d196050fc163aa92e1bbb53f589cdae495
sumbriakrish17-debug <sumbriakrish17@gmail.com>
2026-02-12 00:04:20 +0530

    Implement circular queue with modulo operations

---
+++ circular with modulo.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void display()
+{
+    int i;
+
+    if (front == -1)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Queue elements: ");
+
+    if (rear >= front)
+    {
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue[i]);
+    }
+    else
+    {
+        for (i = front; i < MAX; i++)
+            printf("%d ", queue[i]);
+        for (i = 0; i <= rear; i++)
+            printf("%d ", queue[i]);
+    }
+
+    printf("\n");
+}
+
+void insert(int value)
+{
+    if ((rear + 1) % MAX == front)
+    {
+        printf("Queue Overflow\n");
+        display();
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = rear = 0;
+    }
+    else
+    {
+        rear = (rear + 1) % MAX;
+    }
+
+    queue[rear] = value;
+
+    printf("Inserted: %d\n", value);
+    display();
+}
+
+void delete()
+{
+    if (front == -1)
+    {
+        printf("Queue Underflow\n");
+        display();
+        return;
+    }
+
+    printf("Deleted: %d\n", queue[front]);
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % MAX;
+    }
+
+    display();
+}
+
+int main()
+{
+    int choice, value;
+
+    while (1)
+    {
+        printf("\n1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                insert(value);
+                break;
+
+            case 2:
+                delete();
+                break;
+
+            case 3:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
